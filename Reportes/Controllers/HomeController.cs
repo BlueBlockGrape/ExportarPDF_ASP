0d65943c998e2e87746ea1a5e2f61c711e918c12
BlueBlockGrape <s16120237@alumnos.itsur.edu.mx>
2020-05-11 18:13:06 -0500

    primer commit
    Primer y unico commit

---
+++ Reportes/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using Reportes.Models.ViewModel;
+using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Reportes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string nombre="")
+        {
+            ViewBag.nombre = nombre;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+
+            List<ventas> lista = new List<ventas>();
+
+            string conexion = "Server=localhost; Database=proyectoFotos; uid=root; pwd=root;";
+
+            try
+            {
+                using (var con = new MySqlConnection(conexion))
+                {
+                    con.Open();
+                    string consulta = "Select IdVenta,nCliente,descripcion,precioTotal,fechaCompra from Ventas";
+                    var cmd = new MySqlCommand(consulta, con);
+                    MySqlDataReader lector = cmd.ExecuteReader();
+                    while (lector.Read())
+                    {
+                        var venta = new ventas();
+                        venta.IdVenta = lector[0].ToString();
+                        venta.nCliente = lector[1].ToString();
+                        venta.descripcion = lector[2].ToString();
+                        venta.precioTotal = lector[3].ToString();
+                        venta.fechaCompra = lector[4].ToString();
+                        lista.Add(venta);
+                    }
+                    con.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+
+            return View(lista);
+        }
+
+        public ActionResult Print()
+        {
+
+            return new ActionAsPdf("Report")
+            { FileName = "ReporteVentasAaron.pdf" };
+        }
+    }
+}
---
+++ Reportes/Models/ViewModel/ventas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reportes.Models.ViewModel
+{
+    public class ventas
+    {
+        public string IdVenta { get; set; }
+        public string nCliente { get; set; }
+        public string descripcion { get; set; }
+        public string precioTotal { get; set; }
+        public string fechaCompra { get; set; }
+    }
+}
